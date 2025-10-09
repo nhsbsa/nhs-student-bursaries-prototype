23737fdfc4f41b09654e1aee483d7aadbef340aa
RobertW23 <robert.wilson23@nhs.net>
2025-10-09 15:44:02 +0100

    New CoC P1/P2 journeys for all scenarios

--- app/views/change-of-circumstances/V4/_routes.js
+++ app/views/change-of-circumstances/V4/_routes.js
@@ -24,10 +24,10 @@ router.post('/apply/changes-route', (req, res) => {
     CoCType === 'Other change'
   ) {
 
-    if (assessedOn === 'parent' || assessedOn === 'partner') {
+    if (assessedOn === 'oneParent' || assessedOn === 'twoParents' || assessedOn === 'partner') {
       res.redirect('/change-of-circumstances/V4/apply/summary');
     } else {
-      res.redirect('/change-of-circumstances/V4/apply/summary');
+      res.redirect('/change-of-circumstances/V4/apply/living-with-partner');
     }
 
     // ...Otherwise, stay on this page
@@ -40,19 +40,95 @@ router.post('/apply/changes-route', (req, res) => {
 
 router.post('/apply/living-with-partner-route', (req, res) => {
 
+  const livingWithPartner = req.session.data['livingWithPartner']
+
+  // If user selects no option, stay on this page
+  if (
+    livingWithPartner === 'yes'
+  ) {
+    res.redirect('/change-of-circumstances/V4/apply/partner-name');
+  } else if (
+    livingWithPartner === 'no'
+  ) {
+    res.redirect('/change-of-circumstances/V4/apply/living-with-parents');
+  } else {
+    res.redirect('/change-of-circumstances/V4/apply/living-with-partner');
+  }
+
+})
+
+
+router.post('/apply/living-with-parents-route', (req, res) => {
+
   const livingWithParents = req.session.data['livingWithParents']
 
   // If user selects no option, stay on this page
   if (
-    livingWithParents === 'yes'
+    livingWithParents === 'yesOneParent'
   ) {
-    res.redirect('/change-of-circumstances/V4/apply/partner-name');
+    res.redirect('/change-of-circumstances/V4/apply/parent-name');
+  } else if (
+    livingWithParents === 'yesTwoParents'
+  ) {
+    res.redirect('/change-of-circumstances/V4/apply/parent-1-name');
   } else if (
     livingWithParents === 'no'
   ) {
-    res.redirect('/change-of-circumstances/V4/apply/???');
+    res.redirect('/change-of-circumstances/V4/apply/coc-not-required');
   } else {
-    res.redirect('/change-of-circumstances/V4/apply/living-with-parents');
+    res.redirect('/change-of-circumstances/V4/apply/living-with-partner');
+  }
+
+})
+
+
+// router.post('/apply/who-do-you-live-with-route', (req, res) => {
+
+//   const livingWith = req.session.data['livingWith']
+
+//   // If user selects no option, stay on this page
+//   if (
+//     livingWith === 'parentOrParents'
+//   ) {
+//     res.redirect('/change-of-circumstances/V4/apply/parents-living-together');
+//   }
+//   else if (
+//     livingWith === 'partner'
+//   ) {
+//     res.redirect('/change-of-circumstances/V4/apply/partner-name');
+//   }
+//   else if (livingWith === 'neither'
+//   ) {
+//     res.redirect('/change-of-circumstances/V4/apply/coc-not-required');
+//   }
+//   else {
+//     res.redirect('/change-of-circumstances/V4/apply/who-do-you-live-with');
+//   }
+
+// })
+
+
+router.post('/apply/coc-not-required-route', (req, res) => {
+
+  const cocNotRequiredChoice = req.session.data['cocNotRequiredChoice']
+
+  // If user selects no option, stay on this page
+  if (
+    cocNotRequiredChoice === 'changeAnswers'
+  ) {
+    res.redirect('/change-of-circumstances/V4/apply/who-do-you-live-with');
+  }
+  else if (
+    cocNotRequiredChoice === 'startNewCoC'
+  ) {
+    res.redirect('/change-of-circumstances/V4/apply/changes');
+  }
+  else if (cocNotRequiredChoice === 'returnToDashboard'
+  ) {
+    res.redirect('/change-of-circumstances/V4/apply/dashboard');
+  }
+  else {
+    res.redirect('/change-of-circumstances/V4/apply/coc-not-required');
   }
 
 })
@@ -66,6 +142,48 @@ router.post('/apply/partner-name-route', (req, res) => {
 
 
 router.post('/apply/partner-email-route', (req, res) => {
+
+  res.redirect('/change-of-circumstances/V4/apply/summary')
+
+})
+
+
+router.post('/apply/parent-name-route', (req, res) => {
+
+  res.redirect('/change-of-circumstances/V4/apply/parent-email')
+
+})
+
+
+router.post('/apply/parent-email-route', (req, res) => {
+
+  res.redirect('/change-of-circumstances/V4/apply/summary')
+
+})
+
+
+router.post('/apply/parent-1-name-route', (req, res) => {
+
+  res.redirect('/change-of-circumstances/V4/apply/parent-1-email')
+
+})
+
+
+router.post('/apply/parent-1-email-route', (req, res) => {
+
+  res.redirect('/change-of-circumstances/V4/apply/parent-2-name')
+
+})
+
+
+router.post('/apply/parent-2-name-route', (req, res) => {
+
+  res.redirect('/change-of-circumstances/V4/apply/parent-2-email')
+
+})
+
+
+router.post('/apply/parent-2-email-route', (req, res) => {
 
   res.redirect('/change-of-circumstances/V4/apply/summary')
 
