2a82abd85cc6c12c5e27105feefb1bd459d39846
RobertW23 <robert.wilson23@nhs.net>
2025-12-03 18:54:51 +0000

    Added main partner journey to CoC P1/P2

--- app/views/change-of-circumstances/V4/_routes.js
+++ app/views/change-of-circumstances/V4/_routes.js
@@ -30,7 +30,7 @@ router.post('/apply/status-assessment-relationship-route', (req, res) => {
 
   const relationshipStatus = req.session.data['relationshipStatus']
 
-    if (
+  if (
     relationshipStatus === 'Married or in a civil partnership' ||
     relationshipStatus === 'Separated' ||
     relationshipStatus === 'Divorced' ||
@@ -47,9 +47,59 @@ router.post('/apply/status-assessment-relationship-route', (req, res) => {
 
 router.post('/apply/status-assessment-live-with-parents-route', (req, res) => {
 
-    res.redirect('/change-of-circumstances/V4/apply/status-assessment-cya');
-    
+  res.redirect('/change-of-circumstances/V4/apply/status-assessment-cya');
+
 })
+
+
+router.post('/apply/tasklist-parent-or-partner-details-route', (req, res) => {
+
+  const relationshipStatus = req.session.data['relationshipStatus']
+  // If now married or in a civil partnership, ask for new partner details
+  if (
+    relationshipStatus === 'Married or in a civil partnership'
+  ) {
+    res.redirect('/change-of-circumstances/V4/apply/partner-name');
+  }
+  // Else, go to parent details page
+  else {
+    res.redirect('/change-of-circumstances/V4/apply/parent-1-name');
+  }
+
+})
+
+
+router.post('/apply/partner-name-route', (req, res) => {
+
+  res.redirect('/change-of-circumstances/V4/apply/partner-email')
+
+})
+
+
+router.post('/apply/partner-email-route', (req, res) => {
+
+  res.redirect('/change-of-circumstances/V4/apply/partner-cya')
+
+})
+
+
+router.post('/apply/partner-cya-route', (req, res) => {
+
+  const livingWithPartner = req.session.data['livingWithPartner']
+
+  // If not living with partner, ask for evidence of not living together
+  if (
+    livingWithPartner === 'No'
+  ) {
+    res.redirect('/change-of-circumstances/V4/apply/partner-evidence-of-not-living-with');
+  }
+  // Else, go to tasklist
+  else {
+    res.redirect('/change-of-circumstances/V4/apply/tasklist?allSectionsComplete=true');
+  }
+
+})
+
 
 
 
